1b6426c6395f9e34bc63074e9316cf6f8ef897c1
Megan Christine McClure <mcclme02@luther.edu>
2020-03-10 13:55:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+#what makes a prime number: not negative, not 0, not 1, not a composite number, only divisible by itself and one 
+#range => (-5..-1) => [-5, -4, -3, -2, -1]
+
+def prime?(integer)
+  if integer <= 1 
+    return false 
+  elsif (2..integer-1).each do |i| 
+    if integer % i == 0 
+     return false 
+    end 
+    end 
+   end 
+   true 
+end
